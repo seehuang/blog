05f233d5f6bbe90dee295a9ebe5a280e5f749c80
huangcheng <136970135@qq.com>
2018-12-05 10:15:37 +0800

    es6 module demo

---
+++ exportdefault/util.js
@@ -0,0 +1,26 @@
+
+function a(){
+	console.log('a');
+}
+
+function b(){
+	console.log('b');
+}
+
+function c(){
+	console.log('c');
+}
+
+function d(){
+	console.log('d');
+}
+
+const PI = 3.1415
+
+export default{
+	c:c,
+	d:d,
+	PI:PI
+}
+
+export {a,b,c,d,PI}
